e5b4c1f5047a7287ff560cf45cdf1f2ae11bd5e5
mauricio <mauricio.tp_@outlook.com>
2025-07-08 22:59:03 -0300

    feat: setup fastify

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'member-1' })
-
-const project = projectSchema.parse({ id: 'project-1', ownerId: 'member-1' })
-
-console.log(ability.can('update', project))
