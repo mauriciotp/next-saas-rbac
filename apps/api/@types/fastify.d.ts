14c63d4d096597f15453d4d9bc4c52038b87d9d3
mauricio <mauricio.tp_@outlook.com>
2025-08-06 10:55:35 -0300

    feat: orgs get routes

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import { Member, Organization } from '../generated/prisma'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string,
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
---
+++ apps/api/src/http/routes/orgs/get-organization.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get details from organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              organization: z.object({
+                id: z.uuid(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+                ownerId: z.uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const { organization } = await request.getUserMembership(slug)
+
+        return reply.code(200).send({
+          organization,
+        })
+      },
+    )
+}
